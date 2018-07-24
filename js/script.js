2ffb7d6238375d42a54b6ff11f981c6f4d76ae7a
Jordan Mor <jordanmor@gmail.com>
2018-07-24 15:46:12 -0400

    Create printQuote function

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const quotes = [
       source: "A.A. Milne",
       citation: "Winnie the Pooh",
       year: 1926,
-      genre: ["Children's literature", "Children's Poetry"]
+      tags: ["children's literature", "children's poetry"]
   },
   {
       text: 'We all can dance", he said, "if we find the music that we love.',
@@ -19,7 +19,7 @@ const quotes = [
       source: "J.M. Barrie",
       citation: "Peter Pan",
       year: 1911,
-      genre: ["Children's literature", "Fairy Tales"]
+      tags: ["children's literature", "fairy tales"]
   },
   {
       text: "On the night you were born, the moon smiled with such wonder that the stars peeked in to see you and the night wind whispered, ‘Life will never be the same.’",
@@ -30,33 +30,55 @@ const quotes = [
       source: "J.K. Rowling",
       citation: "Harry Potter and The Chamber of Secrets",
       year: 1998,
-      genre: ["Children's literature", "Fantasy"]
+      tags: ["children's literature", "fantasy"]
   },
   {
     text: "I don't understand it any more than you do, but one thing I've learned is that you don't have to understand things for them to be.",
     source: "Madeleine L'Engle",
     citation: "A Wrinkle in Time",
     year: 1998,
-    genre: ["Children's literature", "Fantasy"]
+    tags: ["children's literature", "fantasy"]
   },
   {
     text: "You're mad, bonkers, completely off your head. But I'll tell you a secret. All the best people are.",
     source: "Lewis Carroll",
     citation: "Alice in Wonderland",
-    genre: ["Children's literature", "Fantasy"]
+    tags: ["children's literature", "fantasy"]
   },
   {
     text: "Little by little does the trick",
     source: "Aesop",
-    genre: ["Fables"]
+    tags: ["fables"]
   }
 ];
+
+const quoteBox = document.getElementById('quote-box');
+
 // Create the getRandomQuote function and name it getRandomQuote
 
 
 
 // Create the printQuote function and name it printQuote
+function printQuote() {
 
+  const quote = quotes[0];
+  let html = '';
+  html += `<p class="quote">${quote.text}</p>`;
+  html += `<p class="source">${quote.source}`;
+  if (quote.citation) {
+    html += `<span class="citation">${quote.citation}</span>`;
+  }
+  if (quote.year) {
+    html += `<span class="year">${quote.year}</span></p>`;
+  }
+  if (quote.tags) {
+    const str = quote.tags.join(', ');
+    html += `<p class="tags">${str}</p>`;
+  }
+  quoteBox.innerHTML = html;
+}
+
+printQuote();
 
 
 // This event listener will respond to "Show another quote" button clicks
