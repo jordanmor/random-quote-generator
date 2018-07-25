07be6407b987457b8e6f0ab99b36d9d19dc579b4
Jordan Mor <jordanmor@gmail.com>
2018-07-25 10:16:57 -0400

    Relabeled 'text' property as 'quote'

--- js/script.js
+++ js/script.js
@@ -6,50 +6,50 @@
 
 const quotes = [
   {
-      text: "How lucky I am to have something that makes saying goodbye so hard.",
+      quote: "How lucky I am to have something that makes saying goodbye so hard.",
       source: "A.A. Milne",
       citation: "Winnie the Pooh",
       year: 1926,
       tags: ["children's literature", "children's poetry"]
   },
   {
-      text: 'We all can dance", he said, "if we find the music that we love.',
+      quote: 'We all can dance", he said, "if we find the music that we love.',
       source: "Giles Andreae",
       citation: "Giraffes Can’t Dance"
   },
   {
-      text: "The moment you doubt whether you can fly, you cease forever to be able to do it",
+      quote: "The moment you doubt whether you can fly, you cease forever to be able to do it",
       source: "J.M. Barrie",
       citation: "Peter Pan",
       year: 1911,
       tags: ["children's literature", "fairy tales"]
   },
   {
-      text: "On the night you were born, the moon smiled with such wonder that the stars peeked in to see you and the night wind whispered, ‘Life will never be the same.’",
+      quote: "On the night you were born, the moon smiled with such wonder that the stars peeked in to see you and the night wind whispered, ‘Life will never be the same.’",
       source: "Nancy Tillman"
   },
   {
-      text: "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+      quote: "It is our choices, Harry, that show what we truly are, far more than our abilities.",
       source: "J.K. Rowling",
       citation: "Harry Potter and The Chamber of Secrets",
       year: 1998,
       tags: ["children's literature", "fantasy"]
   },
   {
-    text: "I don't understand it any more than you do, but one thing I've learned is that you don't have to understand things for them to be.",
+    quote: "I don't understand it any more than you do, but one thing I've learned is that you don't have to understand things for them to be.",
     source: "Madeleine L'Engle",
     citation: "A Wrinkle in Time",
     year: 1998,
     tags: ["children's literature", "fantasy"]
   },
   {
-    text: "You're mad, bonkers, completely off your head. But I'll tell you a secret. All the best people are.",
+    quote: "You're mad, bonkers, completely off your head. But I'll tell you a secret. All the best people are.",
     source: "Lewis Carroll",
     citation: "Alice in Wonderland",
     tags: ["children's literature", "fantasy"]
   },
   {
-    text: "Little by little does the trick",
+    quote: "Little by little does the trick",
     source: "Aesop",
     tags: ["fables"]
   }
@@ -87,12 +87,12 @@ function getRandomQuote(array) {
 }
 
 function printQuote() {
-  const quote = getRandomQuote(quotes);
+  const selectedQuote = getRandomQuote(quotes);
   // Use object destructuring for cleaner code
-  const { text, source, citation, year, tags} = quote;
+  const { quote, source, citation, year, tags} = selectedQuote;
 
   let html = '';
-  html += `<p class="quote">${text}</p>`;
+  html += `<p class="quote">${quote}</p>`;
   html += `<p class="source">${source}`;
   if (citation) {
     html += `<span class="citation">${citation}</span>`;
