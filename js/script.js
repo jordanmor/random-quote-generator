2b14865d591b8ac9a40b9a7eab44211a6572ef67
Jordan Mor <jordanmor@gmail.com>
2018-08-20 19:59:46 -0400

    Use shuffle algorithm to avoid quote repetition

--- js/script.js
+++ js/script.js
@@ -3,34 +3,64 @@
 /*=============-=============-=============-=============
                       GLOBAL VARIABLES
 ===============-=============-=============-===========*/
-
 const quoteBox = document.getElementById('quote-box');
 const body = document.querySelector('body');
 const button = document.getElementById('loadQuote');
-const quoteSpeed = 2000;
+const quoteSpeed = 12000;
+let count = 0;
+let randomQuotesArray = shuffle(quotes);
 
 /*=============-=============-=============-=============
                         FUNCTIONS
 ===============-=============-=============-===========*/
 
-// Random number from 0 up to, but not including, selected number
-const createRandomNum = num => Math.floor(Math.random() * num);
+function avoidRepetition(prevArray, newArray) {
+  const prevQuote = prevArray[prevArray.length - 1];
+  const newQuote = newArray[0];
+  if (prevQuote === newQuote) {
+      // move first quote to end of newArray
+      const firstItem = newArray.shift();
+      newArray.push(firstItem);
+      return newArray;
+  }
 
-const capitalize = str => str.charAt(0).toUpperCase() + str.substring(1);
-
-function randomBgColor() {
-  let r = createRandomNum(256), g = createRandomNum(256), b = createRandomNum(256);
-  body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  return newArray;
 }
 
-function getRandomQuote(array) {
-  const randomIndex = createRandomNum(array.length);
-  return array[randomIndex];
+function shuffle(array) {
+  // make shallow copy of current quotes array
+  let newArr = array.slice();
+  let i = newArr.length, j, currentEl;
+  // While there remain elements to shuffle…
+  while (--i > 0) {
+      // Pick a remaining element…
+      j = Math.floor(Math.random() * i);
+      // And swap it with the current element.
+      currentEl = newArr[j];
+      newArr[j] = newArr[i];
+      newArr[i] = currentEl;
+  }
+  // make sure the newArr does not start with the last quote of the previous randomQuotesArray
+  avoidRepetition(array, newArr);
+  return newArr;
+}
+
+function getRandomQuote() {
+  if(count === randomQuotesArray.length) {
+    randomQuotesArray = shuffle(randomQuotesArray);
+    count = 0;
+  }
+  const quote = randomQuotesArray[count];
+
+  count++;
+
+  return quote;
 }
 
 function printQuote() {
-  const selectedQuote = getRandomQuote(quotes);
+  const selectedQuote = getRandomQuote();
   const { quote, source, citation, year, tags} = selectedQuote;
+  const capitalize = str => str.charAt(0).toUpperCase() + str.substring(1);
 
   let html = '';
   html += `<p class="quote">${quote}</p>`;
@@ -42,6 +72,13 @@ function printQuote() {
   
   quoteBox.innerHTML = html;
   randomBgColor();
+}
+
+function randomBgColor() {
+  const createRandomNum = num => Math.floor(Math.random() * num);
+
+  let r = createRandomNum(256), g = createRandomNum(256), b = createRandomNum(256);
+  body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
 
 /*=============-=============-=============-=============
